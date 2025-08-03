7e88e8470e5635e87951b02d614d4498c9e92d91
Mohammed Abdulrahman <mhiztapresidor@gmail.com>
2025-08-03 16:42:29 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')  # Change this in production
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'shelleng_payroll.db'))
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = os.path.join(basedir, 'uploads')
